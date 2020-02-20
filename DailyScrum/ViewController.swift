2a9ff4500dfe3d509c12bccc840c0e0c58565861
Ben Moore <btmoore40@gmail.com>
2020-02-20 12:39:53 -0500

    Commiting initial XCODE project

---
+++ DailyScrum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyScrum
+//
+//  Created by Ben Moore on 2/20/20.
+//  Copyright © 2020 Ben Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
