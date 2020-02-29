78638e550b65ff76fca23d2f88b693361f6769f9
Ben Moore <btmoore40@gmail.com>
2020-02-29 18:07:47 -0500

    Adding new table view for members

--- DailyScrum/TeamViewController.swift
+++ DailyScrum/TeamViewController.swift
@@ -8,27 +8,29 @@
 
 import UIKit
 
-class TeamViewController: UIViewController {
-
+class TeamViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        teamTable.dataSource = self
+        teamTable.delegate = self
+        teamTable.tableFooterView = UIView(frame: CGRect.zero)
+    }
+    
     @IBAction func closeButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var teamTable: UITableView!
+    var members: [String] = [String](arrayLiteral: "Ben", "Beatrice", "Mitchell")
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return members.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "teamTableCell", for: indexPath)
+        cell.textLabel?.text = members[indexPath.item]
+        return cell
     }
-    */
-
 }
