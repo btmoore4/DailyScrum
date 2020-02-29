14fd4857d5bf2c8f767566bbfb103c69883aadbb
Ben Moore <btmoore40@gmail.com>
2020-02-29 16:16:44 -0500

    Added second screen for Team Selection / Creation

---
+++ DailyScrum/TeamViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TeamViewController.swift
+//  DailyScrum
+//
+//  Created by Ben Moore on 2/29/20.
+//  Copyright © 2020 Ben Moore. All rights reserved.
+//
+
+import UIKit
+
+class TeamViewController: UIViewController {
+
+    @IBAction func closeButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
